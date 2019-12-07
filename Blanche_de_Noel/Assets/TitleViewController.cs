464952070b72c13136b6e0d358c2352821176e7e
KaitoKudou <kaitokudou2468110921@gmail.com>
2019-12-07 14:51:40 +0900

    crete file

---
+++ Blanche_de_Noel/Assets/TitleViewController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleViewController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+private float x = 0.0f;
+public bool isMovementOn = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isMovementOn) x += 0.02f;
+        this.transform.position = new Vector3(x,0,0);
+    }
+}
