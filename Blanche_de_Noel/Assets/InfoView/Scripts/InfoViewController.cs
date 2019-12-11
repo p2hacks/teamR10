20e13ae4c7a8618642accb45c38efd1d50250e24
Takumi Muraishi <b1018104@fun.ac.jp>
2019-12-12 00:14:42 +0900

    add InfoScene

--- Blanche_de_Noel/Assets/InfoView/Scripts/InfoViewController.cs
+++ Blanche_de_Noel/Assets/InfoView/Scripts/InfoViewController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Info : MonoBehaviour
+public class InfoViewController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
