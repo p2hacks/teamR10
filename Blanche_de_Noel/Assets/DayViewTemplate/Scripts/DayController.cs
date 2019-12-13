0f9e4f68f877ebc49bd92d2b61594ba5d395f6ef
yut0124 <b1018133@fun.ac.jp>
2019-12-13 23:11:25 +0900

    Developに結果発表演出を合体

--- Blanche_de_Noel/Assets/DayViewTemplate/Scripts/DayController.cs
+++ Blanche_de_Noel/Assets/DayViewTemplate/Scripts/DayController.cs
@@ -36,6 +36,21 @@ public class DayController : MonoBehaviour
     GameObject buttonChoice3;
     GameObject buttonChoice4;
 
+    /**************** クリスマスの結果発表用変数 ******************/
+    float p_move = 0.0f;
+    float x = 0.0f;
+
+    float fadeOpacity = 0.0f;
+    float snowOpacity = 0.0f;
+    public AudioClip presentSet;
+
+    GameObject Present;
+    GameObject Present_O;
+    GameObject PresentContain;
+    GameObject SnowEffect;
+    GameObject Fader;
+    /*********************************************************/
+
 
     // Start is called before the first frame update
     void Start()
@@ -52,6 +67,23 @@ public class DayController : MonoBehaviour
         buttonChoice2.SetActive(false);
         buttonChoice3.SetActive(false);
         buttonChoice4.SetActive(false);
+
+        /**************** クリスマスの結果発表用 ******************/
+        Present = GameObject.Find("PresentBox");
+        Present_O = GameObject.Find("PresentBox_O");
+        PresentContain = GameObject.Find("Benz");
+
+        Fader = GameObject.Find("Fader");
+        SnowEffect = GameObject.Find("Snow");
+        Material fade = Fader.GetComponent<Renderer>().material;
+        Material snow = SnowEffect.GetComponent<Renderer>().material;
+
+        fade.color = new Color(0.0f, 0.0f, 0.0f, fadeOpacity);
+        snow.color = new Color(0.0f, 0.0f, 0.0f, snowOpacity);
+
+        Present_O.SetActive(false);
+        PresentContain.SetActive(false);
+        /******************************************************/
 
         GAMEMAIN.ResetKoukandoTrigger();
 
@@ -254,8 +286,112 @@ public class DayController : MonoBehaviour
 
     void ChristmasDayResult()
     {
-        SimpleMessage("クリスマスの日の結果発表");
+        switch (turn)
+        {
+            case 0:
+                DisableNextButton();
+                ChristmasDayFadeIn();
+                break;
+            case 1:
+                DisableNextButton();
+                ChristmasDayFadeOut();
+                break;
+            case 2:
+                messageText.SetActive(true);
+                SimpleMessage("クリスマスプレゼントよ！");
+                break;
+            case 3:
+                DisableNextButton();
+                present_move();
+                break;
+            case 4:
+                DisableNextButton();
+                present_open();
+                break;
+            case 5:
+                SimpleMessage("ベンツだ！！");
+                break;
+            default:
+                SimpleMessage("【エラー】このターンに何も割り当てられていません");
+                break;
+        }
     }
+
+    /************************* クリスマスの結果発表用 ここから **************************/
+    void present_move()
+    {
+        Present.gameObject.transform.Translate(0.0f, p_move, 0.0f);
+        Debug.Log("Presentのxは" + Present.transform.position.y);
+        if (Present.transform.position.y > 0.0f)
+        {
+            x = x + 0.001f;
+            p_move = p_move - x * x;
+        }
+        else if (Present.transform.position.y < 0.0f)
+        {
+            p_move = 0.0f;
+            EnableNextButton();
+        }
+    }
+
+    void present_open()
+    {
+        if (Present.transform.position.y < 2.0f)
+        {
+            Present.gameObject.transform.Translate(0.0f, 0.2f, 0.0f);
+        }
+        else if (Present.transform.position.y >= 2.0f)
+        {
+            Present.SetActive(false);
+            Present_O.SetActive(true);
+            if (Present_O.transform.position.y >= 0.0f)
+            {
+                Present_O.gameObject.transform.Translate(0.0f, -0.2f, 0.0f);
+            }
+            EnableNextButton();
+        }
+    }
+
+    void ChristmasDayFadeIn()
+    {
+        messageText.SetActive(false);
+        if (snowOpacity <= 1.0f)
+        {
+            fadeOpacity = 1.0f;
+            SnowEffect.GetComponent<Renderer>().material.color = new Color(255, 255, 255, snowOpacity);
+            Fader.GetComponent<Renderer>().material.color = new Color(0, 0, 0, fadeOpacity);
+            snowOpacity = snowOpacity + 0.01f;
+        }
+        else if (snowOpacity >= 1.0f)
+        {
+            snowOpacity = 1.0f;
+            EnableNextButton();
+        }
+    }
+
+    void ChristmasDayFadeOut()
+    {
+        if ((snowOpacity >= 0.0f) && (fadeOpacity >= 0.0f))
+        {
+            SnowEffect.GetComponent<Renderer>().material.color = new Color(255, 255, 255, snowOpacity);
+            Fader.GetComponent<Renderer>().material.color = new Color(0, 0, 0, fadeOpacity);
+            snowOpacity = snowOpacity - 0.01f;
+            fadeOpacity = fadeOpacity - 0.01f;
+        }
+        else if ((snowOpacity <= 0.0f) && (fadeOpacity <= 0.0f))
+        {
+            snowOpacity = 0.0f;
+            fadeOpacity = 0.0f;
+            EnableNextButton();
+        }
+    }
+
+    void present_contain()
+    {
+        Present_O.SetActive(false);
+
+    }
+    /********************************** クリスマスの結果発表用 ここまで *************************************/
 
     /**************************************************
      *　　　　　　　　　　1日の流れ編集エリア　　　　　　　　　*
