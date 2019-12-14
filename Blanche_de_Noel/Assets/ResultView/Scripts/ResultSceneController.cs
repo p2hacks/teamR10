6851c69831ed7fd0f615eb764b0b218a1d0f4fb3
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-12-14 16:01:20 +0900

    new2

--- Blanche_de_Noel/Assets/ResultView/Scripts/ResultSceneController.cs
+++ Blanche_de_Noel/Assets/ResultView/Scripts/ResultSceneController.cs
@@ -16,19 +16,19 @@ public class ResultSceneController : MonoBehaviour
         
     }
 
-    void ResultPresentMove()
-    {
-        Present.gameObject.transform.Translate(0.0f, p_move, 0.0f);
-        Debug.Log("Presentのxは" + Present.transform.position.y);
-        if (Present.transform.position.y > 0.0f)
-        {
-            x = x + 0.001f;
-            p_move = p_move - x * x;
-        }
-        else if (Present.transform.position.y < 0.0f)
-        {
-            p_move = 0.0f;
-            EnableNextButton();
-        }
-    }
+    //void ResultPresentMove()
+    //{
+    //    Present.gameObject.transform.Translate(0.0f, p_move, 0.0f);
+    //    Debug.Log("Presentのxは" + Present.transform.position.y);
+    //    if (Present.transform.position.y > 0.0f)
+    //    {
+    //        x = x + 0.001f;
+    //        p_move = p_move - x * x;
+    //    }
+    //    else if (Present.transform.position.y < 0.0f)
+    //    {
+    //        p_move = 0.0f;
+    //        EnableNextButton();
+    //    }
+    //}
 }
