374dff1344b865d70e4963e2d64486ae40ae05e2
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-12-14 19:44:39 +0900

    update

--- Blanche_de_Noel/Assets/ResultView/Scripts/ResultSceneController.cs
+++ Blanche_de_Noel/Assets/ResultView/Scripts/ResultSceneController.cs
@@ -8,31 +8,55 @@ public class ResultSceneController : MonoBehaviour
     GameObject Saku;
     GameObject Switch;
 
+    GameObject blackBackGround;
+
+    int BackHideDelay = 0;
+    bool isBackHideDelay = true;
+
     // Start is called before the first frame update
-    void Start() {
+    void Start()
+    {
         if (ChooseDifficultyController.difficultyBorder > 0)
         {
             Saku = GameObject.Find("Saku");
-            Switch = GameObject.Find("Switch");
             ResultSaku();
         }
         else
         {
-
+            Switch = GameObject.Find("Switch");
+            ResultSwitch();
         }
+
+        blackBackGround = GameObject.Find("BlackBackground");
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Debug.Log(ChooseDifficultyController.difficultyBorder);
+
+        if (isBackHideDelay) BackHideDelay++;
+
+        if(BackHideDelay > 20) //20フレーム（0.33秒）たったら隠す画像を非アクティブにする
+        {
+            blackBackGround.SetActive(false);
+            isBackHideDelay = false;
+        }
     }
 
     public void ResultSaku()
     {
         Saku.gameObject.transform.Translate(0.0f, 0.0f, -20.0f);
-        Switch.gameObject.transform.Translate(0.0f, 0.0f, 20.0f);
     }
+
+    public void ResultSwitch()
+    {
+        Switch.gameObject.transform.Translate(0.0f, 0.0f, -20.0f);
+    }
+
+    
 
     //void ResultPresentMove()
     //{
