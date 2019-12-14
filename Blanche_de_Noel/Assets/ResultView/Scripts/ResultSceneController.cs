06270c7fd2db5f76a1d8e0c65f4c8a7c6cab3e26
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-12-14 15:59:12 +0900

    new

---
+++ Blanche_de_Noel/Assets/ResultView/Scripts/ResultSceneController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultSceneController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ResultPresentMove()
+    {
+        Present.gameObject.transform.Translate(0.0f, p_move, 0.0f);
+        Debug.Log("Presentのxは" + Present.transform.position.y);
+        if (Present.transform.position.y > 0.0f)
+        {
+            x = x + 0.001f;
+            p_move = p_move - x * x;
+        }
+        else if (Present.transform.position.y < 0.0f)
+        {
+            p_move = 0.0f;
+            EnableNextButton();
+        }
+    }
+}
