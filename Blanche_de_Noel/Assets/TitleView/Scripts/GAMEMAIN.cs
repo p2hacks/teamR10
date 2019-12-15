020371bbc2d7d6090111ba84e0b6f0fa2a91c597
yut0124 <b1018133@fun.ac.jp>
2019-12-15 09:03:26 +0900

    シナリオを更新

--- Blanche_de_Noel/Assets/TitleView/Scripts/GAMEMAIN.cs
+++ Blanche_de_Noel/Assets/TitleView/Scripts/GAMEMAIN.cs
@@ -10,6 +10,9 @@ public class GAMEMAIN : MonoBehaviour
     private static int sportKoukando = 0; // 主人公のスポーツパラメータ
     private static int day = 1; //　経過日数
     private static bool koukandoTrigger = false;
+    private static bool studyKoukandoTrigger = false;
+    private static bool sportKoukandoTrigger = false;
+    private static bool gameKoukandoTrigger = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,6 +25,7 @@ public class GAMEMAIN : MonoBehaviour
     {
         
     }
+
     public static int GetDay() => day; // 経過日数のゲッター
     public static void SetDay(int n) => day += n; // 経過日数のセッター
 
@@ -38,32 +42,32 @@ public class GAMEMAIN : MonoBehaviour
     public static int GetStudyKoukando() => studyKoukando; //勉強パラメータのゲッター
     public static void AddStudyKoukando(int n) //勉強パラメータのセッター//引数にどれだけ上がるかを渡す
     {
-        if (koukandoTrigger)
+        if (studyKoukandoTrigger)
         {
             studyKoukando += n;
-            koukandoTrigger = false;
+            studyKoukandoTrigger = false;
         }
     }
 
     public static int GetGameKoukando() => gameKoukando; //　ゲームパラメータのゲッター
     public static void AddGameKoukando(int n) //ゲームパラメータのセッター//引数にどれだけ上がるかを渡す
     {
-        if (koukandoTrigger)
+        if (gameKoukandoTrigger)
         {
             gameKoukando += n;
-            koukandoTrigger = false;
+            gameKoukandoTrigger = false;
         }
     }
 
     public static int GetSportKoukando() => sportKoukando; // スポーツパラメータのゲッター
     public static void AddSportKoukando(int n) //スポーツパラメータのセッター//引数にどれだけ上がるかを渡す
     {
-        if (koukandoTrigger)
+        if (sportKoukandoTrigger)
         {
             sportKoukando += n;
-            koukandoTrigger = false;
+            sportKoukandoTrigger = false;
         }
     }
 
-    public static void ResetKoukandoTrigger() => koukandoTrigger = true;
+    public static void ResetKoukandoTrigger() => koukandoTrigger = gameKoukandoTrigger = sportKoukandoTrigger = studyKoukandoTrigger = true; //トリガーを元に戻す
 }
